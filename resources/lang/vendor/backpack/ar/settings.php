c62812a72469db7f90317c9cb95e0f5307bf26a1
Mansour Alnasser <alnassre.it@gmail.com>
2021-02-04 17:29:37 +0300

    added arabic

--- resources/lang/vendor/backpack/ar/settings.php
+++ resources/lang/vendor/backpack/ar/settings.php
@@ -9,10 +9,10 @@ return [
     | The following language lines are used for Laravel Backpack - Settings
     |
     */
-    'name'             => 'Name',
-    'value'            => 'Value',
-    'description'      => 'Description',
-    'setting_singular' => 'setting',
-    'setting_plural'   => 'settings',
+    'name'             => 'الاسم',
+    'value'            => 'القيمة',
+    'description'      => 'الوصف',
+    'setting_singular' => 'الإعداد',
+    'setting_plural'   => 'الإعدادات',
 
 ];
--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -15,7 +15,7 @@
 
 
 <!-- News , Pages -->
-<li class="nav-item nav-dropdown">
+<li class="nav-item nav-dropdown" style="display: none;">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-newspaper-o"></i>{{ __('base.News')}}</a>
     <ul class="nav-dropdown-items">
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-file-o'></i> <span>{{ __('base.Pages')}}</span></a></li>
@@ -33,8 +33,10 @@
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-cog"></i> {{ __('base.Settings') }}</a>
     <ul class="nav-dropdown-items">
         <li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}\"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
-        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i> Logs</a></li>
-        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon la la-hdd-o'></i> Backups</a></li>
+
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon la la-hdd-o'></i> {{ __('base.Backups') }}</a></li>
         <li class='nav-item'><a class='nav-link' href='{{ backpack_url('setting') }}'><i class='nav-icon la la-cog'></i> {{ __('base.Settings') }} </a></li>
+
+        <li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i> Logs</a></li>
     </ul>
 </li>
