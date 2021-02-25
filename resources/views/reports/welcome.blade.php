c4a3f300f801e4701d174b43a6072f55d16f5db5
Mansour Alnasser <alnassre.it@gmail.com>
2021-02-25 20:19:58 +0300

    added Meneses\LaravelMpdf

--- resources/views/reports/welcome.blade.php
+++ resources/views/reports/welcome.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<html dir="rtl">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -7,9 +7,8 @@
 
 <body>
     <h2>Noto Sans</h2>
-    <p style="font-family: 'Noto Sans', sans-serif;">Гордей Беляев</p>
     <h2>Amiri</h2>
-    <p style="font-family: 'Amiri', serif;">عنوان</p>
+    <p style="font-family:  tahoma;">عنوان</p>
     <p style="font-family: 'arial', serif;">عنوان</p>
 </body>
 
