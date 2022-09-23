29559e44206fb9757570d68130cd87dd5e9c1f30
Mansour Alnasser <alnassre.it@gmail.com>
2022-09-23 11:58:23 +0300

    added payments

--- app/Http/Requests/PaymentRequest.php
+++ app/Http/Requests/PaymentRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class PaymentRequest extends FormRequest
 {
@@ -25,9 +26,20 @@ class PaymentRequest extends FormRequest
      */
     public function rules()
     {
+
+        $request = request();
         return [
-            'course_id' => 'required|',
-            'student_id' => 'required|',
+            'course_id' => 'required',
+            'student_id' => [
+                'required',
+                // Rule::unique('student_id', 'type'),
+                Rule::unique('payments')->where(function ($query) use ($request) {
+                    return
+                        $query->where('course_id', $request->course_id)
+                        ->where('type', $request->type)
+                        ->where('student_id', $request->student_id);
+                })
+            ],
             'source' => 'required',
             'type' => 'required',
             'amount' => 'required|numeric',
@@ -54,7 +66,8 @@ class PaymentRequest extends FormRequest
     public function messages()
     {
         return [
-            //
+            'student_id.unique' => 'الرصيد لهذا الطالب+الدورة الدراسية+نوع الرصيد : موجود من قبل', // custom message
+
         ];
     }
 }
