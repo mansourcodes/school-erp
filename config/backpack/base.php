1d177832a24d2e089d6fb47bcfb351afbdb63140
Mansour Alnasser <alnassre.it@gmail.com>
2022-12-22 17:07:04 +0300

    added dashboard statistics

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -24,7 +24,7 @@ return [
     // ----
 
     // Project name. Shown in the window title.
-    'project_name' => 'حوزة',
+    'project_name' => env('APP_NAME'),
 
     // When clicking on the admin panel's top-left logo/name,
     // where should the user be redirected?
@@ -71,7 +71,7 @@ return [
     // ------
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => ' <b>حوزة</b> Cloud',
+    'project_logo'   => env('APP_NAME'),
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -11,6 +11,13 @@
             // 'button_text' => trans('backpack::base.logout'),
         ],
         [
+            'type' => 'card', // the kind of widget to show
+            'class' => '', // optional
+            'content' => [
+                'body' => '<h3>إختصارات</h3>',
+            ], // the content of that widget (some are string, some are array)
+        ],
+        [
             'type' => 'div',
             'class' => 'row',
             'content' => [
@@ -18,7 +25,7 @@
                 [
                     'type' => 'card',
                     'wrapper' => ['class' => 'col-sm-12 col-md-4'], // optional
-                    'class' => 'card bg-primary text-white', // optional
+                    'class' => 'card bg-cyan text-white', // optional
                     'content' => [
                         // 'header' => 'Some card title', // optional
                         'body' => "<a class='h1 d-block py-5 m-0 text-white' href='" . backpack_url('course') . "' ><i class='nav-icon la la-campground'></i>" . trans('course.courses') . '</a>',
@@ -36,11 +43,45 @@
                 [
                     'type' => 'card',
                     'wrapper' => ['class' => 'col-sm-12 col-md-4'], // optional
-                    'class' => 'card bg-primary text-white', // optional
+                    'class' => 'card bg-gray-400 text-white', // optional
                     'content' => [
                         // 'header' => 'Some card title', // optional
-                        'body' => "<a class='h1 d-block py-5 m-0 text-white' href='" . backpack_url('payment') . "' ><i class='nav-icon la la-file-invoice-dollar'></i>" . trans('account.payments') . '</a>',
+                        'body' => "<a class='h1 d-block py-5 m-0 text-dark' href='" . backpack_url('payment') . "' ><i class='nav-icon la la-file-invoice-dollar'></i>" . trans('account.payments') . '</a>',
                     ],
+                ],
+                [
+                    'type' => 'card',
+                    'wrapper' => ['class' => 'col-sm-12 col-md-4'], // optional
+                    'class' => 'card bg-green text-white', // optional
+                    'content' => [
+                        // 'header' => 'Some card title', // optional
+                        'body' => "<a class='h1 d-block py-5 m-0 text-white' href='" . backpack_url('add_attend_by_date') . "' ><i class='nav-icon la la-check-square'></i>" . trans('attend.add_attend_w_date') . '</a>',
+                    ],
+                ],
+            ], // widgets end
+        ],
+    
+        [
+            'type' => 'card', // the kind of widget to show
+            'class' => '', // optional
+            'content' => [
+                'body' => '<h3>إحصائيات</h3>',
+            ], // the content of that widget (some are string, some are array)
+        ],
+    
+        [
+            'type' => 'div',
+            'class' => 'row',
+            'content' => [
+                // widgets start
+                [
+                    'type' => 'progress_white',
+                    'class' => 'card mb-2',
+                    'value' => '11.456',
+                    'description' => 'عدد الطلاب',
+                    'progress' => 11, // integer
+                    'progressClass' => 'progress-bar bg-primary',
+                    'hint' => 'ارقام غير صحيحة مجرد مثال',
                 ],
             ], // widgets end
         ],
