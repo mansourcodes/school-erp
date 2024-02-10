d59cde5a8e7a94e64dae7d86a82e021c1398ceac
Mansour Alnasser <alnassre.it@gmail.com>
2024-02-10 10:32:49 +0300

    fix

--- database/migrations/2023_10_28_095111_change_student_name_to_name_into_students_table.php
+++ database/migrations/2023_10_28_095111_change_student_name_to_name_into_students_table.php
@@ -26,7 +26,7 @@ class ChangeStudentNameToNameIntoStudentsTable extends Migration
     public function down()
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->renameColumn('name', 'name');
+            $table->renameColumn('name', 'student_name');
         });
     }
 }
