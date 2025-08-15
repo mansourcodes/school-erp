96a15be20db94b414c47d318e8886044434b7eba
Mansour Alnasser <alnassre.it@gmail.com>
2025-08-15 20:51:19 +0300

    debug

--- app/Helpers/BriefMarkHelper.php
+++ app/Helpers/BriefMarkHelper.php
@@ -87,7 +87,7 @@ class BriefMarkHelper
           ];
         } catch (\Throwable $th) {
           //throw $th;
-          dd($gain_mark, $student_mark);
+          dd($gain_mark, $student_mark, $gain_mark->mark ?? 'n/a');
         }
       }
     }
